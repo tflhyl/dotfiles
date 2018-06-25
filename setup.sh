09a09fe8e79ab785ef2203c94b903ea279f14961
tflhyl <tflhyl@gmail.com>
2018-06-25 22:46:13 +0800

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# TODO install brew if needed
+brew install rbenv
+
+# bash
+cp .bash_profile ~/.
+cp .alias ~/.
+cp .inputrc ~/.
+source ~/.bash_profile
+
+# git
+cp .gitconfig ~/.
+echo "Enter your git username:"
+read git_username
+git config --global user.name $git_username
+echo "Enter your git email:"
+read git_email
+git config --global user.email $git_email
+
+# vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp .vimrc ~/.
+mkdir -p ~/.vim/colors && cp flattown.vim ~/.vim/colors/
+vim -u ~/.vimrc +PluginInstall +qall
+
+# iTerm
+open nakafurano.itermcolors
+
